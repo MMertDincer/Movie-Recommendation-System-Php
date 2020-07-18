ee0a2fd931c5f830112b1ca79887ca5f655b1d29
MMertDincer <mert.dincer05@gmail.com>
2020-07-18 11:18:31 +0300

    ++Movie Recommender

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Movies;
+use App\RatedMovie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\Object_;
+use function Sodium\add;
 
 class HomeController extends Controller
 {
@@ -23,6 +29,34 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $data['rated'] = DB::table('rated_movies')
+            ->where('user_id',Auth::id())
+            ->where('movie_rate', '>=', 6.5)
+            ->get();
+
+        $data['movies'] = [];
+        $i = 0;
+        foreach ($data['rated'] as $rated){
+            array_push($data['movies'],DB::table('movies')
+                ->where('id',$rated->movie_id)
+                ->first ());
+            $data['movies'][$i]->movie_rate = $rated->movie_rate;
+            $output = exec('cd C:\Users\Vols\Desktop\MovieRS\ && py movie_recommender_completed.py "'.$data['movies'][$i]->movie_title.'"');
+            $like_movies = json_decode($output);
+            $data['like_movies'] = [];
+            foreach($like_movies as $like_movie){
+                array_push($data['like_movies'], DB::table('movies')
+                    ->select('movie_slug','movie_title')
+                    ->where('movie_title',$like_movie)
+                    ->first());
+            }
+            $data['movies'][$i]->like_movies = $data['like_movies'];
+            $i++;
+        }
+
+
+        return view('home', compact('data'));
     }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,37 @@
 @extends('frontend.layout')
 
 @section('content')
-<div class="container content">
-    <div class="row mt-4 justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container content">
+        <div class="row mt-4 justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-
+                        @foreach($data['movies'] as $movie)
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    {{$movie->movie_title}}
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    @foreach($movie->like_movies as $like_movie)
+                                    <a class="dropdown-item" href="{{route('movies.Detail' ,$like_movie->movie_slug)}}">{{$like_movie->movie_title}}</a>
+                                    @endforeach
+                                </div>
+                            </div>
+                            <br>
+                            @endforeach
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 @endsection
